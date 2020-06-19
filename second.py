5299974cf50e1c7cec5fc5808f93be6c96479ab7
ananthumangalathu <ananthumangalathu@gmail.com>
2020-06-19 16:11:00 +0530

    function add new data added

--- second.py
+++ second.py
@@ -2,10 +2,67 @@ import tkinter as tk
 from tkinter import *
 from tkinter import Message ,Text
 import os
+import cv2 as cv
+import mysql.connector
+import csv
+
+mydb=mysql.connector.connect(host="127.0.0.1",port="3306",user="root",passwd="Sarath@1998",database="mine")
+mycursor=mydb.cursor()
+
 
 root=Tk()
 
 root.geometry("700x400")
+
+def add_new():
+    Id=(text2.get())
+    Name=(text3.get())
+    Email=(text4.get())
+    if(is_number(Id) and Name.isalpha()):
+        cam = cv.VideoCapture(0)
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        detector=cv.CascadeClassifier(harcascadePath)
+        sampleNum=0
+        while(True):
+            ret, img = cam.read()
+            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            faces = detector.detectMultiScale(gray, 1.3, 5)
+            for (x,y,w,h) in faces:
+                cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)        
+                #incrementing sample number 
+                sampleNum=sampleNum+1
+                #saving the captured face in the dataset folder TrainingImage
+                cv.imwrite("Images\ "+Name +"."+Id +'.'+ str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
+                #display the frame
+                cv.imshow('frame',img)
+            #wait for 100 miliseconds 
+            if cv.waitKey(100) & 0xFF == ord('q'):
+                break
+            # break if the sample number is morethan 100
+            elif sampleNum>60:
+                break
+        cam.release()
+        cv.destroyAllWindows() 
+        res = "Images Saved for ID : " + Id +" Name : "+ Name
+        row = (Id, Name,Email)
+        sql="INSERT INTO student_database (Id,Name,Email) VALUES (%s,%s,%s)"
+        mycursor.execute(sql,row)
+        mydb.commit()
+        mycursor.execute("SELECT * FROM student_database")
+        data=mycursor.fetchall()
+        with open('StudentDetails\StudentDetails.csv','w') as f:
+            writer = csv.writer(f, delimiter=',')
+            writer.writerow(["Id","Name","Email"])
+            writer.writerows(data)
+        f.close()
+        message.configure(text= res)
+    else:
+        if(is_number(Id)):
+            res = "Enter Alphabetical Name"
+            message.configure(text= res)
+        if(Name.isalpha()):
+            res = "Enter Numeric Id"
+            message.configure(text= res)
 
 def quit():
     root.destroy()
