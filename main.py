cbdae19d8c9c0c091ef03af73750b4023a5cf080
ananthumangalathu <ananthumangalathu@gmail.com>
2020-06-19 14:23:00 +0530

    basic gui

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import Message ,Text
+
+window=Tk() 
+window.geometry("700x400")
+
+button1=tk.Button(window,text="ADD NEW",width=40,height=2,font=("times new roman",20),bg="red",fg='white',command="")
+button1.place(x=50,y=10)
+
+button2=tk.Button(window,text="MARK ATTENDANCE",width=40,height=2,font=("times new roman",20),bg="red",fg='white',command="")
+button2.place(x=50,y=110)
+
+button3=tk.Button(window,text="ATTENDANCE SHEET",width=40,height=2,font=("times new roman",20),bg="red",fg='white',command="")
+button3.place(x=50,y=210)
+
+button4=tk.Button(window,text="QUIT",width=40,height=2,font=("times new roman",20),bg="red",fg='white',command=window.destroy)
+button4.place(x=50,y=310)
+
+window.mainloop()
